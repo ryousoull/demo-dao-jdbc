5b14a62b363f3ecb0ec172e8c3d25cfe4fc2e949
Adriel Felipe <adrielfelipevieira@gmail.com>
2026-09-02 00:15:05 -0300

    alteração do Program e criação da classe Seller

--- demo-dao-jdbc/src/model/application/Program.java
+++ demo-dao-jdbc/src/model/application/Program.java
@@ -1,10 +1,16 @@
 package model.application;
 
 import model.entities.Departamet;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     static void main() {
         Departamet obj = new Departamet(1, "Books");
+
+        Seller seller = new Seller(21, "Bob", "bobGamil@", new Date(), 3000.0, obj);
         System.out.println(obj);
+        System.out.println(seller);
     }
 }
